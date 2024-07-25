8175df3ec945be3115232b1bfdc8a05d592de689
vade <vade@vade.info>
2024-07-25 11:54:41 -0400

    Initial Commit

---
+++ LibAVExtensionHost/LibAVExtensionHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibAVExtensionHostApp.swift
+//  LibAVExtensionHost
+//
+//  Created by Anton Marini on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LibAVExtensionHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LibAVExtensionHostTests/LibAVExtensionHostTests.swift
@@ -0,0 +1,16 @@
+//
+//  LibAVExtensionHostTests.swift
+//  LibAVExtensionHostTests
+//
+//  Created by Anton Marini on 7/25/24.
+//
+
+import Testing
+
+struct LibAVExtensionHostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
